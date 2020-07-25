438906e311f627f85ade54ccfb006da7bef8bc9d
thomcz <thomas.czogalik@gmx.de>
2020-07-25 11:23:05 +0200

    remove unused code

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, BehaviorSubject, Subject } from 'rxjs';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 import { HelloWorldRestControllerService } from 'src/generated-backend/services/hello-world-rest-controller.service';
 
 import { Greeting } from 'src/generated-backend/models/greeting';
@@ -11,12 +10,8 @@ import { Greeting } from 'src/generated-backend/models/greeting';
   styleUrls: ['./app.component.scss']
 })
   export class AppComponent {
-  //   hello:Observable<Object>
-  //   constructor(private httpClient: HttpClient) { 
-  //     this.hello = this.httpClient.get(`http://localhost:8080/api/hello`);
-  //    }
-
-  hello:Observable<Greeting>
+    
+  hello: Observable<Greeting>
   constructor(private greetingService: HelloWorldRestControllerService) { 
     this.hello = this.greetingService.hello({
         name:'Thomas'
